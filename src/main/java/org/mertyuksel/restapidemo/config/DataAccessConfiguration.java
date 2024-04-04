b67830d195c5cb0cd96587c70258464be14079cb
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2024-04-04 13:50:28 +0300

    Review'da yer alan değişiklikler
    - Bean'i Config paketi altında DataAccessConfiguration sınıfı ile aynı adı taşıyan bir .java dosyasına taşıdım.
    - persistence-sqlite.properties dosyasındaki url tanımını file-based olarak değiştirdim.

---
+++ src/main/java/org/mertyuksel/restapidemo/config/DataAccessConfiguration.java
@@ -0,0 +1,13 @@
+@configuration
+public class DataAccessConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("driverClassName"));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUsername(env.getProperty("user"));
+        dataSource.setPassword(env.getProperty("password"));
+        return dataSource;
+    }
+}
