b5028c191bbdea7382ac507994b23fb94514451f
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2024-04-18 13:11:51 +0300

    Import'lar eklendi

--- src/main/java/org/mertyuksel/restapidemo/RestApiDemoApplication.java
+++ src/main/java/org/mertyuksel/restapidemo/RestApiDemoApplication.java
@@ -1,8 +1,5 @@
 package org.mertyuksel.restapidemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class RestApiDemoApplication {
 
