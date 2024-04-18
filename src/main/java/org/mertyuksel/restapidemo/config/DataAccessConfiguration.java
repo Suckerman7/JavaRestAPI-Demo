2351ce00f504329a1e4b0fcc628a819436607e80
Suckerman7 <43205411+Suckerman7@users.noreply.github.com>
2024-04-18 14:43:43 +0300

    Update DataAccessConfiguration.java
    Kaldırmam gereken kalkmamış, hemen kaldırayım.

--- src/main/java/org/mertyuksel/restapidemo/config/DataAccessConfiguration.java
+++ src/main/java/org/mertyuksel/restapidemo/config/DataAccessConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 @Configuration
 public class DataAccessConfiguration {
 
@@ -17,7 +16,6 @@ public class DataAccessConfiguration {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(env.getProperty("driverClassName"));
         dataSource.setUrl(env.getProperty("url"));
-        dataSource.setUsername(env.getProperty("username"));
         dataSource.setUsername(env.getProperty("user"));
         dataSource.setPassword(env.getProperty("password"));
         return dataSource;
