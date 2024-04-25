ea61ebfcd78078928cabf9ae9e2dfdaad7e2d43e
Suckerman7 <mrty.yksl@gmail.com>
2024-04-25 13:03:24 +0300

    Revert "Update DataAccessConfiguration.java"
    This reverts commit ab190fb570b274ebbbcb217afeae8d17871cdb16.

--- src/main/java/org/mertyuksel/restapidemo/config/DataAccessConfiguration.java
+++ src/main/java/org/mertyuksel/restapidemo/config/DataAccessConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-public static Configuration getConfiguration()
-
 @Configuration
 public class DataAccessConfiguration {
 
