d45e41b0737d55678e9672320b23d0b0ee31c471
Suckerman7 <mrty.yksl@gmail.com>
2024-05-16 17:15:13 +0300

    Dockerize etme çalışmaları
    IntelliJ'den Docker'a bağlanamıyorum maalesef.

--- src/main/java/org/mertyuksel/restapidemo/controller/RestApiDemoController.java
+++ src/main/java/org/mertyuksel/restapidemo/controller/RestApiDemoController.java
@@ -2,15 +2,15 @@ package org.mertyuksel.restapidemo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class RestApiDemoController {
 
     @GetMapping("/hello")
     @ResponseStatus(HttpStatus.OK)
-    public String hello() {
-        return "Hello World with Docker!";
+    public String HelloWorld() {
+        return "Hello World, with Docker!";
     }
 }
