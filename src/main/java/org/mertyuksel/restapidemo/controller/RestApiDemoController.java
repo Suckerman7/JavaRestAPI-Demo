345109a0368a643c8aaa29ff3da245887a372327
Suckerman7 <mrty.yksl@gmail.com>
2024-05-23 13:25:53 +0300

    Update RestApiDemoController.java

--- src/main/java/org/mertyuksel/restapidemo/controller/RestApiDemoController.java
+++ src/main/java/org/mertyuksel/restapidemo/controller/RestApiDemoController.java
@@ -1,16 +1,17 @@
 package org.mertyuksel.restapidemo.controller;
 
-import org.springframework.http.HttpStatus;
+import org.mertyuksel.restapidemo.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApiDemoController {
 
+    @Autowired
+    private HelloWorldService helloWorldService;
     @GetMapping("/hello")
-    @ResponseStatus(HttpStatus.OK)
-    public String HelloWorld() {
+    public String hello() {
         return "Hello World, with Docker!";
     }
 }
